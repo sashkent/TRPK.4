05f67122887db239e50444a2d200a6eecc1610af
sashkent <67151683+sashkent@users.noreply.github.com>
2023-01-22 03:33:08 +0800

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    $(':submit').click(function (e) {
+        if (($('.number').val() == "") || ($('.base').val() == "")) {
+            alert('Заполните поля!');
+            $('.result').text("");
+        }
+        else
+        {
+            var number = $('.number').val();
+            var base = $('.base').val();
+            $('.result').text(Math.log(number) / Math.log(base));
+        }
+    });
+});
+
